943c8c9a84011ebec9ee07b80364270927139cc6
leon Boakye-Adjei <leon_adjei@hotmail.com>
2020-09-07 13:42:35 +0100

    edit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const albumRoutes = require('./routes/albums');
 const userRoutes = require('./routes/user');
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://leonboakye:${process.env.PASSWORD}@ds035816.mlab.com:35816/heroku_rq0g67ck`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(`mongodb+srv://LeonBoakye:${process.env.PASSWORD}@cluster0-l6zcb.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -33,9 +33,7 @@ app.use((req, res, next) => {
     })
 })
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'));
-}
+
 
 
 const port = process.env.PORT || 2402;
